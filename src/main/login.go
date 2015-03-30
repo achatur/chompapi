55837c96acf10af66ad38980f09f2efb6158b608
Amir Chatur <amir@thechompapp.com>
2015-03-30 01:34:48 +0000

    This commit is saving the cookie generation and sending at a good spot.  Cookie generates properly and is being returned back to the caller in the normal HTTP way (cookie in header).  I have not validated session persistance yet.  We'll need a sessions_table in the db for this.  Will add a temporary table for trial the next save.

--- src/main/login.go
+++ src/main/login.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	//"hash"
 	"net/http"
+	"text/template"
 )
 
 type LoginInput struct {
@@ -26,8 +27,25 @@ func doLogin(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("%+v", input)
 		dbPassword := generatePassword(input.Username, []byte(input.Password))
 		fmt.Printf("dbPass= %+x\n", dbPassword)
+		validated := validatePassword(input.Username, []byte(input.Password), string(dbPassword))
 		fmt.Printf("answer = %v", validatePassword(input.Username, []byte(input.Password), string(dbPassword)))
+		if validated == true {
+			sess, err := globalSessions.SessionStart(w, r)
+			if err != nil {
+				fmt.Printf("Error %v\n", err)
+			}
+			fmt.Printf("Session = %v\n", sess)
+			//defer sess.SessionRelease(w)
+			sess.Set("username", input.Username)
+			username := sess.Get("username")
+			fmt.Printf("Username = %v\n", username)
 
+			//t, _ := template.ParseFiles("login.gtpl")
+			//t.Execute(w, nil)
+		} else {
+			fmt.Printf("Login Failed")
+			//return error
+		}
 	default:
 		//	fmt.Fprintf(w, "Wrong Format")
 		w.WriteHeader(http.StatusMethodNotAllowed)
--- src/main/main.go
+++ src/main/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/astaxie/beego/session"
+	//_ "github.com/astaxie/session/providers/memory"
 	"html"
 	"log"
 	"net/http"
 )
+
+var globalSessions *session.Manager
 
 func main() {
 	http.HandleFunc("/register", doRegister)
@@ -16,4 +20,13 @@ func main() {
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func init() {
+	var err error
+	globalSessions, err = session.NewManager("memory", `{"cookieName":"gosessionid","gclifetime":3600}`)
+	if err != nil {
+		fmt.Printf("Error")
+	}
+	go globalSessions.GC()
 }
