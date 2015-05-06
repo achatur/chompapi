b38d873d236a241cd1522dfff06691ef86c29019
Amir Chatur <amir@thechompapp.com>
2015-05-06 02:39:49 +0000

    Changed port to be environment variable based

--- src/main/main.go
+++ src/main/main.go
@@ -7,6 +7,8 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 //Global Variable
@@ -16,8 +18,12 @@ func main() {
 	http.HandleFunc("/register", doRegister)
 	http.HandleFunc("/login", doLogin)
 
-	//log.Fatal(http.ListenAndServeTLS(":8443", "/home/amir.chatur/working/playground/gen_cert/thechompapp.com.pem", "/home/amir.chatur/working/playground/gen_cert/thechompapp.com.key.pem", nil))
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	port := os.Getenv("PORT")
+	if strings.Contains(string(port), "443") {
+		log.Fatal(http.ListenAndServeTLS(":"+port, "/home/amir.chatur/working/playground/gen_cert/thechompapp.com.pem", "/home/amir.chatur/working/playground/gen_cert/thechompapp.com.key.pem", nil))
+	} else {
+		log.Fatal(http.ListenAndServe(":8000", nil))
+	}
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
