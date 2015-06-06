94d32ab453e8950a82261707e0abf25e7f9da699
Amir Chatur <amir@thechompapp.com>
2015-06-06 13:53:44 -0500

    Nested photo id struct inside of user struct

--- db/db.go
+++ db/db.go
@@ -19,7 +19,11 @@ type RegisterInput struct {
 	Lname    	 string
 	Phone     	 string
 	Hash     	 string
-	PhotoLocation	 string
+	Photo
+}
+
+type Photo struct {
+	ID 	int
 }
 
 func GetUserInfo(username string) (map[string]string, error) {
@@ -104,33 +108,4 @@ func IsValid(s sql.NullString) string {
 		fmt.Println("s is not valid")
 		return s.String
 	}
-}
-
-func structToMap(i interface{}) (values url.Values) {
-	values = url.Values{}
-	iVal := reflect.ValueOf(i).Elem()
-	typ := iVal.Type()
-	for i := 0; i < iVal.NumField(); i++ {
-		f := iVal.Field(i)
-		// You ca use tags here...
-		// tag := typ.Field(i).Tag.Get("tagname")
-		// Convert each type into a string for the url.Values string map
-		var v string
-		switch f.Interface().(type) {
-		case int, int8, int16, int32, int64:
-			v = strconv.FormatInt(f.Int(), 10)
-		case uint, uint8, uint16, uint32, uint64:
-			v = strconv.FormatUint(f.Uint(), 10)
-		case float32:
-			v = strconv.FormatFloat(f.Float(), 'f', 4, 32)
-		case float64:
-			v = strconv.FormatFloat(f.Float(), 'f', 4, 64)
-		case []byte:
-			v = string(f.Bytes())
-		case string:
-			v = f.String()
-		}
-		values.Set(typ.Field(i).Name, v)
-	}
-	return
 }
