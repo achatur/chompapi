0a943ee9af20cfff678e58569ce3170057848d7c
Amir Chatur <amir@thechompapp.com>
2015-08-10 02:18:07 -0500

    Changed for fix

--- src/cmd/chompapi/register/register.go
+++ src/cmd/chompapi/register/register.go
@@ -88,7 +88,7 @@ func DoRegister(w http.ResponseWriter, r *http.Request) {
 		igStore 	 := new(db.IgStore)
 		igStore.UserID = input.UserID
 		igStore.IgMediaID = "fake"
-		igStore.IgCreatedTime = time.Now()
+		igStore.IgCreatedTime = time.Unix(time.now)
 		err = igStore.UpdateLastPull()
 		if err != nil {
 			fmt.Printf("Could not update table\n")
