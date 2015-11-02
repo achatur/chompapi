1b9f1258f61ec6e411981e1392f66322e4a42008
Amir Chatur <amir@thechompapp.com>
2015-11-02 20:47:21 +0000

    change..

--- src/cmd/chompapi/main.go
+++ src/cmd/chompapi/main.go
@@ -225,7 +225,7 @@ func main() {
 
 	//this is how you write a query parameter capture uri
 	router.Queries("token", "{token}").HandlerFunc(AppHandler{appContext: context, h: me.Instagram}.SessionAuth(AppHandler{context, me.Instagram}.ServerHttp))
-	router.Queries("token", "cd80fd58ebf00b8bf7ff2a4020fa2a9b", "code", "{code:.*").HandlerFunc(AppHandler{appContext: context, h: me.Instagram}.SessionAuth(AppHandler{context, me.Instagram}.ServerHttp))
+	router.Queries("token", "{token}", "code", "{code:.*").HandlerFunc(AppHandler{appContext: context, h: me.Instagram}.SessionAuth(AppHandler{context, me.Instagram}.ServerHttp))
 	router.Queries("token", "{token}", "error", "{error").HandlerFunc(AppHandler{appContext: context, h: me.Instagram}.SessionAuth(AppHandler{context, me.Instagram}.ServerHttp))
 	// router.Queries("code", "{code}").HandlerFunc(AppHandler{appContext: context, h: me.Instagram}.SessionAuth(AppHandler{context, me.Instagram}.ServerHttp))
 	router.Queries("error", "{error}").HandlerFunc(AppHandler{appContext: context, h: me.Instagram}.SessionAuth(AppHandler{context, me.Instagram}.ServerHttp))
