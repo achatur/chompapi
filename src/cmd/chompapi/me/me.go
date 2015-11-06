a43577533c6b4bfa7533f9cdc178c1c27163e2ca
Amir Chatur <amir@thechompapp.com>
2015-11-06 01:06:36 +0000

    fixes for last push

--- src/cmd/chompapi/me/me.go
+++ src/cmd/chompapi/me/me.go
@@ -474,10 +474,10 @@ func getInstagramToken(instagramTokenReq *InstagramTokenRequest) (string, error)
 	// resp, body, errs := request.Post(iurl).Set("Accept", "application/json").Send(instagramTokenReq).End()
 	// resp, body, errs := request.Post(iurl).Send(send).End()
 	resp, errs := http.PostForm("https://api.instagram.com/oauth/access_token",
-    url.Values{"client_id": {"%s", instagramTokenReq.ClientId},
-    			"client_secret": {"%s", instagramTokenReq.ClientSecret},
-    			"grant_type":{"authorization_code"}, "redirect_uri": {"%s", instagramTokenReq.RedirectUri},
-    			"code":{"%s", instagramTokenReq.Code}})
+    url.Values{"client_id": {instagramTokenReq.ClientId},
+    			"client_secret": {instagramTokenReq.ClientSecret},
+    			"grant_type":{"authorization_code"}, "redirect_uri": {instagramTokenReq.RedirectUri},
+    			"code":{instagramTokenReq.Code}})
 	if errs != nil {
 		fmt.Printf("something went wrong in get %v", errs)
 		return "nil", errs
